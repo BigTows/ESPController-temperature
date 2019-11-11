22982a645a0f1e0c5441b0d251bc3c4da8bdbbf7
Alexander Chapchuk <BigTows@yandex.ru>
2019-11-11 14:08:42 +0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -29,8 +29,8 @@ float t = 0.0;
 // The value will quickly become too large for an int to store
 unsigned long previousMillis = 0;    // will store last time DHT was updated
 
-// Updates DHT readings every 15 seconds
-const long interval = 15000;
+// Updates DHT readings every 5 minutes
+const long interval = 1000 * 60 * 5;
 
 void setup() {
   // Serial port for debugging purposes
